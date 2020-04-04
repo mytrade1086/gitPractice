272d2aee3a636102062b75c87d8419f21db8b103
unknown <mytrade1086@gmail.com>
2020-04-04 12:56:46 +0530

    two lines in this class

---
+++ src/main/java/gitProject/gitProject/Checkoutminusminus.java
@@ -0,0 +1,15 @@
+package gitProject.gitProject;
+
+public class Checkoutminusminus {
+
+	//Demonstrate git checkout --
+	//Suppose we have 2 lines in this class.
+	//We add line 3 and save
+	//Now we think we dont need line 3. so we can just 
+	
+	
+	//LINE1
+	//LINE2
+	
+	
+}
