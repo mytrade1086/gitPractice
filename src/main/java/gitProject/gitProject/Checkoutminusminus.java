00b420b927e59c593dc0fd0d4372658303065f13
unknown <mytrade1086@gmail.com>
2020-04-04 13:06:11 +0530

    line 1 and 2 seen

--- src/main/java/gitProject/gitProject/Checkoutminusminus.java
+++ src/main/java/gitProject/gitProject/Checkoutminusminus.java
@@ -3,9 +3,13 @@ package gitProject.gitProject;
 public class Checkoutminusminus {
 
 	//Demonstrate git checkout --
-	//Suppose we have 2 lines in this class.
-	//We add line 3 and save
-	//Now we think we dont need line 3. so we can just 
+	//Suppose we have 2 lines in this class and commited on remote
+	
+	
+	//We add line 3 in local and save
+	//Now we think we don't need line 3.
+	//1. delete line 3 manually or 
+	//2. git checkout -- <filename>  Observe the clipse after that . Line 3 will be gone 
 	
 	
 	//LINE1
