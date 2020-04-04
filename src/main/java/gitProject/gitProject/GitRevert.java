42510486109dad27751c0e916901dbdcfc288739
unknown <mytrade1086@gmail.com>
2020-04-04 13:31:17 +0530

    some documents

--- src/main/java/gitProject/gitProject/GitRevert.java
+++ src/main/java/gitProject/gitProject/GitRevert.java
@@ -10,5 +10,13 @@ public class GitRevert {
 	
 	
 	//line 2 added    https://github.com/mytrade1086/gitPractice/commit/3c25ad282af25411797529c9a812027147330e12
-
+	
+	
+	
+	/*
+	 * if we want to go back to state 1 we can do git revert
+	 * 5f19e2b4055464b230aad851725bd084f1bccd04
+	 * 
+	 * It will create one more commit
+	 */
 }
